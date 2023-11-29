a1d1c7248846fae4a584cb6c63d338e827fd769a
an4lex <an4lex@gmail.com>
2023-11-29 23:50:41 +0200

    Initial Commit

---
+++ EcoFill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 29.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EcoFill/EcoFillApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoFillApp.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 29.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct EcoFillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
