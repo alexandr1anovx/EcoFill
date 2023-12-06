19183c9b6bd1ab72b86396724bed61e8d6296611
an4lex <an4lex@gmail.com>
2023-12-06 17:06:55 +0200

    Decorated Profile screen.

--- EcoFill/Screens/ProfileScreen.swift
+++ EcoFill/Screens/ProfileScreen.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ProfileScreen: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State private var isPresentedLogOutConfirmation: Bool = false
+  
+  var body: some View {
+    NavigationStack {
+      UserDataPreview()
+      NavigationLink("Редагувати") {
+        UserPrivateDataPreview()
+      }
+      .fontWeight(.medium)
+      .tint(.customBlack)
+      .buttonStyle(.borderedProminent)
+      .padding(.top, 5)
+      
+      List {
+        /// Settings section
+        Section {
+          NavigationLink {
+            SettingsScreen()
+          } label: {
+            SettingsCell(title: "Налаштування", iconName: "gear", background: .customBlack)
+          }
+        } footer: {
+          Text("Змінити тему застосунку, налаштувати повідомлення.")
+        }
+        
+        /// Log Out section
+        Section {
+          Button("Вийти", systemImage: "door.right.hand.open") {
+            isPresentedLogOutConfirmation.toggle()
+          }
+          .foregroundStyle(.red)
+          .confirmationDialog("Log Out", isPresented: $isPresentedLogOutConfirmation) {
+            Button("Log Out", role: .destructive) {
+              // Log Out action ...
+            }
+          } message: {
+            Text("Are you sure you want to log out?")
+          }
+        } footer: {
+          Text("Вийти з вашого облікового запису.")
+        }
+      }
+      .listStyle(.insetGrouped)
+      .padding(.top, 20)
     }
 }
 
