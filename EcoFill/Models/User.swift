5b8d642f6983153aee6cd2995c2e746084270b74
an4lex <an4lex@gmail.com>
2023-12-06 16:34:07 +0200

    A model has been created that contains the user's personal information.

---
+++ EcoFill/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 05.12.2023.
+//
+
+import Foundation
+
+struct User: Codable {
+  var firstName = ""
+  var lastName = ""
+  var phoneNumber = ""
+  var email = ""
+}
