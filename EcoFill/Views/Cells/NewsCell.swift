25039774935eeefc1ec53b022767f982339174dc
an4lex <an4lex@gmail.com>
2023-12-11 22:32:37 +0200

    Created a cell for the news list.

---
+++ EcoFill/Views/Cells/NewsCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewsCell.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 10.12.2023.
+//
+
+import SwiftUI
+
+struct NewsCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsCell()
+}
