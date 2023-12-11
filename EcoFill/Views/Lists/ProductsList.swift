e590afddb45d6027ac752007ad83618b15618618
an4lex <an4lex@gmail.com>
2023-12-11 22:55:54 +0200

    Created a list of produts for the section called 'Наші продукти'.

---
+++ EcoFill/Views/Lists/ProductsList.swift
@@ -0,0 +1,63 @@
+//
+//  ProductsList.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 04.12.2023.
+//
+
+import SwiftUI
+
+struct ProductsList: View {
+  @State private var products = [Product]()
+  @State private var isShowingProductPreview: Bool = false
+  @State private var selectedProduct: Product?
+  
+  var body: some View {
+    NavigationStack {
+      if products.isEmpty {
+        ContentUnavailableView("The list of products is empty.",
+                               systemImage: "fuelpump",
+                               description: Text("Please, check your internet connection."))
+      } else {
+        List(products) { product in
+          ProductCell(product: product)
+            .onTapGesture {
+              selectedProduct = product
+              isShowingProductPreview = true
+            }
+            .sheet(item: $selectedProduct) { product in
+              ProductPreview(product: product) {
+                // Set to nil when user presses cancellation button.
+                selectedProduct = nil
+              }
+            }
+        }
+        .listStyle(.insetGrouped)
+        .listRowSpacing(20)
+        .navigationTitle("Продукти")
+        .navigationBarTitleDisplayMode(.inline)
+      }
+    }
+    .task {
+      await fetchProductData()
+    }
+  }
+  
+  func fetchProductData() async {
+    guard let url = URL(string: "http://localhost:3000/products") else {
+      print("Error: Invalid URL")
+      return
+    }
+    do {
+      let (data, _) = try await URLSession.shared.data(from: url)
+      let decodedData = try JSONDecoder().decode([Product].self, from: data)
+      products = decodedData
+    } catch {
+      print("Error fetching or decoding data: \(error)")
+    }
+  }
+}
+
+#Preview {
+  ProductsList()
+}
