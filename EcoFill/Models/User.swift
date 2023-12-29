bf1eefdb391da00bb799b9d711031a156c5c663f
an4lex <an4lex@gmail.com>
2023-12-29 15:56:44 +0200

    The 'User' model was refactored.

--- EcoFill/Models/User.swift
+++ EcoFill/Models/User.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-struct User: Codable {
-  var firstName = ""
-  var lastName = ""
-  var phoneNumber = ""
-  var email = ""
+struct User: Identifiable, Codable {
+  let id: String
+  let fullName: String
+  let email: String
+  let city: String
+}
+
+extension User {
+  static var MOCK_User = User(id: NSUUID().uuidString,
+                              fullName: "Alex Andrianov",
+                              email: "an4lex@gmail.com",
+                              city: "Kyiv")
 }
