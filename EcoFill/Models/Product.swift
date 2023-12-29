d24e07071ec0e1af8784410d16710ba574a96b0f
an4lex <an4lex@gmail.com>
2023-12-29 15:58:41 +0200

    Test products data were deleted.

--- EcoFill/Models/Product.swift
+++ EcoFill/Models/Product.swift
@@ -13,8 +13,3 @@ struct Product: Decodable, Identifiable {
   let description: String
   let price: Double
 }
-
-let testProducts: [Product] = [
-  Product(id: 1, title: "A-95", description: "Тестовий опис A-95", price: 49.50),
-  Product(id: 2, title: "A-92", description: "Тестовий опис A-92", price: 45.20)
-]
