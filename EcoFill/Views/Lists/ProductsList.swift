f2340986ed263f02888f63330f1b2204c9f87c27
an4lex <an4lex@gmail.com>
2024-01-05 17:37:33 +0200

    Whole logic moved to view model. Now data is taken from firebase instead of the JSON-server.

--- EcoFill/Views/Lists/ProductsList.swift
+++ EcoFill/Views/Lists/ProductsList.swift
@@ -8,18 +8,19 @@
 import SwiftUI
 
 struct ProductsList: View {
-  @State private var products = [Product]()
-  @State private var isShowingProductPreview: Bool = false
+  @State private var isShowingProductPreview = false
   @State private var selectedProduct: Product?
+  
+  @ObservedObject var dataViewModel = FirestoreDataViewModel()
   
   var body: some View {
     NavigationStack {
-      if products.isEmpty {
+      if dataViewModel.products.isEmpty {
         ContentUnavailableView("The list of products is empty.",
                                systemImage: "fuelpump",
                                description: Text("Please, check your internet connection."))
       } else {
-        List(products) { product in
+        List(dataViewModel.products) { product in
           ProductCell(product: product)
             .onTapGesture {
               selectedProduct = product
@@ -35,25 +36,29 @@ struct ProductsList: View {
         .navigationBarTitleDisplayMode(.inline)
       }
     }
-    .task {
-      await fetchProductData()
-    }
-  }
-  
-  func fetchProductData() async {
-    guard let url = URL(string: "http://localhost:3000/products") else {
-      print("Error: Invalid URL")
-      return
-    }
-    do {
-      let (data, _) = try await URLSession.shared.data(from: url)
-      let decodedData = try JSONDecoder().decode([Product].self, from: data)
-      products = decodedData
-    } catch {
-      print("Error fetching or decoding data: \(error)")
+    .onAppear {
+      dataViewModel.fetchProductsData()
     }
   }
 }
+    //    .task {
+    //      await fetchProductData()
+    //    }
+    
+  //  func fetchProductData() async {
+  //    guard let url = URL(string: "http://localhost:3000/products") else {
+  //      print("Error: Invalid URL")
+  //      return
+  //    }
+  //    do {
+  //      let (data, _) = try await URLSession.shared.data(from: url)
+  //      let decodedData = try JSONDecoder().decode([Product].self, from: data)
+  //      products = decodedData
+  //    } catch {
+  //      print("Error fetching or decoding data: \(error)")
+  //    }
+  //  }
+
 
 #Preview {
   ProductsList()
