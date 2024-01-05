aa783f4c70854a4c5b38d451ff4a6cd1f8bd59d9
an4lex <an4lex@gmail.com>
2024-01-05 17:31:16 +0200

    Whole logic moved to view model.

--- EcoFill/Views/Lists/LocationsList.swift
+++ EcoFill/Views/Lists/LocationsList.swift
@@ -8,50 +8,53 @@
 import SwiftUI
 
 struct LocationsList: View {
-  @State private var locations = [Location]()
+  @ObservedObject var dataViewModel = FirestoreDataViewModel()
   
   var body: some View {
     NavigationStack {
-      if locations.isEmpty {
+      if dataViewModel.locations.isEmpty {
         ContentUnavailableView("The list of locations is empty.",
                                systemImage: "mappin",
                                description: Text("Please, check your internet connection."))
       } else {
-        List(locations) { location in
+        List(dataViewModel.locations) { location in
           LocationCell(location: location)
         }
         .listRowSpacing(20)
         .listStyle(.insetGrouped)
-        .navigationTitle("Локації")
+        .navigationTitle("Locations")
         .navigationBarTitleDisplayMode(.inline)
       }
     }
-    .task {
-      // This method asynchronously loads all locations from the server side and lists them.
-      await fetchLocationsData()
-    }
-  }
-  
-  
-  func fetchLocationsData() async {
-    // Check url existence.
-    guard let url = URL(string: "http://localhost:3000/locations") else {
-      fatalError("URL doesn't work properly!")
-    }
-    // Fetch data from URL.
-    do {
-      let (data,_) = try await URLSession.shared.data(from: url)
-      
-      // Decode our data with JSONDecoder.
-      let decodedData = try JSONDecoder().decode([Location].self, from: data)
-      
-      // Fill in our locations array with decoded data.
-      locations = decodedData
-    } catch {
-      print("Error fetching data: \(error)")
+    .onAppear {
+      dataViewModel.fetchLocationsData()
     }
   }
 }
+//    .task {
+//      // This method asynchronously loads all locations from the server side and lists them.
+//      await fetchLocationsData()
+//    }
+  
+//  func fetchLocationsData() async {
+//    // Check url existence.
+//    guard let url = URL(string: "http://localhost:3000/locations") else {
+//      fatalError("URL doesn't work properly!")
+//    }
+//    // Fetch data from URL.
+//    do {
+//      let (data,_) = try await URLSession.shared.data(from: url)
+//      
+//      // Decode our data with JSONDecoder.
+//      let decodedData = try JSONDecoder().decode([Location].self, from: data)
+//      
+//      // Fill in our locations array with decoded data.
+//      locations = decodedData
+//    } catch {
+//      print("Error fetching data: \(error)")
+//    }
+//  }
+
 
 #Preview {
   LocationsList()
