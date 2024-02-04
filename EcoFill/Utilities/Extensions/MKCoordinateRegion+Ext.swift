2cb813d7237e9b502d5d390cca7f3230de795238
an4lex <an4lex@gmail.com>
2024-02-05 00:07:08 +0200

    Created an extension for MKCoordinateRegion.

---
+++ EcoFill/Utilities/Extensions/MKCoordinateRegion+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  MKCoordinateRegion+Extension.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 02.02.2024.
+//
+
+import Foundation
+import MapKit
+
+extension MKCoordinateRegion: Equatable {
+  public static func == (lhs: MKCoordinateRegion, rhs: MKCoordinateRegion) -> Bool {
+    if lhs.center.latitude == rhs.center.latitude &&
+        lhs.span.latitudeDelta == rhs.span.latitudeDelta &&
+        lhs.span.longitudeDelta == rhs.span.longitudeDelta {
+      return true
+    } else {
+      return false
+    }
+  }
+}
