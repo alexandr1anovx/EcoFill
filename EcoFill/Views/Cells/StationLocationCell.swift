402593fd81df09cca8a9cbf04fe7838b5652f73f
an4lex <an4lex@gmail.com>
2024-02-08 23:08:37 +0200

    The 'LocationCell' was renamed.

---
+++ EcoFill/Views/Cells/StationLocationCell.swift
@@ -0,0 +1,57 @@
+//
+//  LocationCell.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 01.12.2023.
+//
+
+import SwiftUI
+
+struct StationLocationCell: View {
+  var station: Station
+  
+  var body: some View {
+    HStack {
+      VStack(alignment: .leading, spacing:12) {
+        Text(station.street)
+          .font(.callout)
+          .fontWeight(.medium)
+          .fontDesign(.rounded)
+          .foregroundStyle(.accent)
+          .lineLimit(2)
+        
+        Text(station.city)
+          .font(.caption)
+          .fontWeight(.semibold)
+          .fontDesign(.rounded)
+          .foregroundStyle(.defaultReversed)
+        
+        Text(station.schedule)
+          .font(.system(size: 12,
+                        weight: .medium,
+                        design: .rounded))
+          .foregroundStyle(.gray)
+      }
+      
+      Spacer()
+      
+      RoundedRectangle(cornerRadius: 8)
+        .fill(.defaultSystem)
+        .frame(width: 90, height: 40, alignment: .center)
+        .overlay {
+          HStack(spacing:5) {
+            Image(systemName: "location")
+              .imageScale(.medium)
+            Text("Map")
+              .font(.callout)
+              .onTapGesture {
+                // action
+              }
+          }
+          .padding(.horizontal)
+        }
+    }
+  }
+}
+
+#Preview { StationLocationCell(station: .testStation) }
