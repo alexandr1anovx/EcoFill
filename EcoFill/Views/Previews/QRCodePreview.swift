92ddbdbfdb36a228b2a0c2dfa8b7cd5ca0b890b5
an4lex <an4lex@gmail.com>
2024-02-08 23:26:09 +0200

    Created a QR code which is formed with name and email.

--- EcoFill/Views/Previews/QRCodePreview.swift
+++ EcoFill/Views/Previews/QRCodePreview.swift
@@ -6,21 +6,78 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct QRCodePreview: View {
-  @Binding var isShowingQRCodePreview: Bool
+  // MARK: - Properties
+  //  @Binding var isShowingQRCodePreview: Bool
+  
+  @State private var name: String = ""
+  @State private var emailAddress: String = ""
+  @EnvironmentObject var authViewModel: AuthViewModel
+  
+  let context = CIContext()
+  let filter = CIFilter.qrCodeGenerator()
+  
+  // MARK: - body
   var body: some View {
-    VStack {
-      Text("QR Code Preview")
-        .font(.title2).bold()
+    NavigationStack {
+      if let currentUser = authViewModel.currentUser {
+        VStack(alignment: .leading,spacing:15) {
+          HStack {
+            Text("Full name:")
+              .foregroundStyle(.gray)
+              .font(.footnote)
+              .fontWeight(.semibold)
+            
+            Text(currentUser.fullName)
+              .foregroundStyle(.defaultReversed)
+              .font(.callout)
+              .fontWeight(.semibold)
+          }
+          
+          Divider()
+          
+          HStack {
+            Text("Email address:")
+              .foregroundStyle(.gray)
+              .font(.footnote)
+              .fontWeight(.medium)
+            Text(currentUser.email)
+              .foregroundStyle(.defaultReversed)
+              .font(.callout)
+              .fontWeight(.medium)
+          }
+        }
+        .padding()
+        .navigationTitle("QR Code")
+        .navigationBarTitleDisplayMode(.inline)
+        
+        Image(uiImage: generateQRCode(from: "\(currentUser.fullName)\n\(currentUser.email)"))
+          .resizable()
+          .interpolation(.none)
+          .scaledToFit()
+          .frame(width: 170, height: 170)
+          .padding(.top,15)
+        
+        Spacer()
+      }
     }
-    .presentationDetents([.height(400)])
-    .presentationCornerRadius(15)
-    .presentationBackground(.defaultBackground)
-    .presentationDragIndicator(.visible)
+  }
+  
+  func generateQRCode(from string: String) -> UIImage {
+    filter.message = Data(string.utf8)
+    if let outputImage = filter.outputImage {
+      if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+        return UIImage(cgImage: cgImage)
+      }
+    }
+    return UIImage(systemName: "xmark.circle") ?? UIImage()
   }
 }
 
 #Preview {
-  QRCodePreview(isShowingQRCodePreview: .constant(true))
+  QRCodePreview()
+    .environmentObject(AuthViewModel())
 }
