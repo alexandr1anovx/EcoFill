2ac07b89f7d35631daab2f5929256d0ee924b29e
an4lex <an4lex@gmail.com>
2024-02-11 23:48:25 +0200

    MapItemPreview has been redesigned.

--- EcoFill/Views/Previews/MapItemPreview.swift
+++ EcoFill/Views/Previews/MapItemPreview.swift
@@ -14,31 +14,69 @@ struct MapItemPreview: View {
   
   // MARK: - body
   var body: some View {
-    VStack(alignment: .center, spacing:15) {
-      Text(station.name)
-        .font(.headline)
-        .foregroundStyle(.defaultReversed)
-      
-      Divider()
-      
-      Text(station.address)
-        .font(.subheadline)
-        .foregroundStyle(.gray)
-        .multilineTextAlignment(.leading)
-      
-      Text(station.schedule)
-        .font(.callout).bold()
-        .foregroundStyle(.defaultReversed)
-        .multilineTextAlignment(.leading)
-      
-      Button("Get direction") {
-        // route
+    ZStack {
+      LinearGradient(colors: [.defaultSystem], startPoint: .topLeading, endPoint: .bottomTrailing)
+        .ignoresSafeArea()
+      // MARK: - Street info
+      HStack {
+        VStack(alignment: .leading,spacing: 15) {
+          // Location info
+          VStack(alignment: .leading,spacing:10) {
+            Text(station.name)
+              .font(.headline)
+              .fontWeight(.semibold)
+            Text(station.address)
+              .font(.callout)
+              .fontWeight(.medium)
+              .foregroundStyle(.gray)
+          }
+          
+          // MARK: - Fuels
+          FuelsStack(station: station)
+          .padding(.top,8)
+          
+          // MARK: - Payment methods
+          HStack(spacing:8) {
+            Text("Pay with: ")
+              .font(.callout)
+              .fontWeight(.medium)
+              .foregroundStyle(.gray)
+            Image("visa")
+              .resizable()
+              .frame(width:45,height:45)
+            Text("or ")
+              .font(.headline)
+              .foregroundStyle(.defaultReversed)
+            Image("cash")
+              .resizable()
+              .frame(width:45,height:45)
+          }
+          .padding(.top,4)
+          
+          // MARK: - Work time
+          HStack {
+            Text("Work time:")
+              .font(.callout)
+              .fontWeight(.medium)
+              .foregroundStyle(.gray)
+            Text(station.schedule)
+              .font(.headline)
+          }
+          // MARK: - Get Direction
+          Button("Get direction", systemImage: "figure.walk") {
+            
+          }
+          .font(.headline)
+          .fontWeight(.medium)
+          .buttonStyle(.borderedProminent)
+          .shadow(radius:8)
+          .padding(.top,7)
+        }// main
+        Spacer()
       }
-      .fontWeight(.medium)
-      .tint(.blue)
-      .buttonStyle(.borderedProminent)
-      .padding(.top,15)
+      .padding(.horizontal,20)
     }
+    
   }
 }
 
@@ -46,3 +84,35 @@ struct MapItemPreview: View {
 #Preview {
   MapItemPreview(station: .testStation)
 }
+
+
+
+//var body: some View {
+//  VStack(alignment: .center, spacing:15) {
+//    Text(station.name)
+//      .font(.headline)
+//      .foregroundStyle(.defaultReversed)
+//
+//    Divider()
+//
+//    Text(station.address)
+//      .font(.subheadline)
+//      .foregroundStyle(.gray)
+//      .multilineTextAlignment(.leading)
+//
+//    Text(station.schedule)
+//      .font(.callout).bold()
+//      .foregroundStyle(.defaultReversed)
+//      .multilineTextAlignment(.leading)
+//
+//    Text("A-95: \(station.address)")
+//
+//    Button("Get direction") {
+//      // route
+//    }
+//    .fontWeight(.medium)
+//    .tint(.blue)
+//    .buttonStyle(.borderedProminent)
+//    .padding(.top,15)
+//  }
+//}
