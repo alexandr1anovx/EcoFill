e8f6cb0041a76b536c8178e39d8830d6963c6cef
an4lex <an4lex@gmail.com>
2024-02-22 09:58:45 +0200

    The 'ProductsList' was renamed to 'FuelsList'.

---
+++ EcoFill/Views/Lists/FuelsList.swift
@@ -0,0 +1,35 @@
+//
+//  ProductsList.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 04.12.2023.
+//
+
+import SwiftUI
+
+struct FuelsList: View {
+  // MARK: - Properties
+  @EnvironmentObject var firestoreVM: FirestoreViewModel
+  var selectedCity: String
+  
+  var filteredStations: [Station] {
+    firestoreVM.stations.filter { $0.city == selectedCity }
+  }
+  
+  var body: some View {
+    VStack {
+      if firestoreVM.stations.isEmpty {
+        CustomUnavailableView()
+      } else {
+        ForEach(filteredStations.prefix(1), id: \.self) { station in
+          ScrollableFuelsStack(station: station)
+        }
+      }
+    }
+  }
+}
+
+#Preview {
+  FuelsList(selectedCity: "Mykolaiv")
+    .environmentObject(FirestoreViewModel())
+}
