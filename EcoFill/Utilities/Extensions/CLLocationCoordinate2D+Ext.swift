ce6ed2b251ee6ac0c197854558ae3ee795bd604c
an4lex <an4lex@gmail.com>
2024-02-24 23:46:35 +0200

    Created a 'CLLocationCoordinate2D' extension with user location.

---
+++ EcoFill/Utilities/Extensions/CLLocationCoordinate2D+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  CLLocationCoordinate2D+Ext.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 18.02.2024.
+//
+
+import Foundation
+import MapKit
+
+extension CLLocationCoordinate2D {
+  static let userLocation = CLLocationCoordinate2D(
+    latitude: 46.959843,
+    longitude: 32.012848)
+}
