7c83869116ee8a550ade12aa0dfda442acde5373
an4lex <an4lex@gmail.com>
2024-02-29 23:40:03 +0200

    Deleted unused service.

--- EcoFill/Models/Service.swift
+++ EcoFill/Models/Service.swift
@@ -8,28 +8,21 @@
 import Foundation
 
 struct Service: Identifiable {
-  let id = UUID()
-  let title: String
-  let description: String
-  let image: String
+  var id = UUID()
+  var title: String
+  var description: String
+  var imageName: String
 }
 
 let services: [Service] = [
-  Service(title: "Products",
-          description: "Watch prices.",
-          image: "fuelpump.circle.fill"),
   
   Service(title: "Support",
-          description: "Send feedback to improve our service.",
-          image: "24.square.fill"),
-  
-  Service(title: "Food", description: "Choose food to eat.", image: "fork.knife.circle.fill")
+          description: "Send a feedback about our service.",
+          imageName: "24.square.fill"),
 ]
 
 extension Service {
   enum ServiceType: String {
-    case products = "Products"
     case support = "Support"
-    case food = "Food"
   }
 }
