6781382dca8ff56e514eaad1e087a5646723f7bd
an4lex <an4lex@gmail.com>
2024-02-29 23:42:29 +0200

    Removed the 'MOCK_User' data.

--- EcoFill/Models/User.swift
+++ EcoFill/Models/User.swift
@@ -8,15 +8,8 @@
 import Foundation
 
 struct User: Identifiable, Codable {
-  let id: String
-  let fullName: String
-  let email: String
-  let city: String
-}
-
-extension User {
-  static var MOCK_User = User(id: NSUUID().uuidString,
-                              fullName: "Alex Andrianov",
-                              email: "an4lex@gmail.com",
-                              city: "Kyiv")
+  var id: String
+  var fullName: String
+  var email: String
+  var city: String
 }
