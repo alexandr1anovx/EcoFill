195a593271c34b19921cc3e536487b0302a2a0ab
an4lex <an4lex@gmail.com>
2024-03-09 23:36:29 +0200

    Removed unnecessary code and changed the Station model properties.

--- EcoFill/Models/Station.swift
+++ EcoFill/Models/Station.swift
@@ -8,23 +8,18 @@
 import Foundation
 import MapKit
 
-struct Station: Decodable, Identifiable, Equatable, Hashable {
-  var id = UUID() // unique ID
-  
+struct Station: Decodable, Identifiable, Hashable {
+  var id = UUID()
   var city: String
-  // fuels
   var euroA95: Double
   var euroDP: Double
   var gas: Double
-  // coordinates
   var latitude: Double
   var longitude: Double
-  // details
   var name: String
   var postalCode: String
   var schedule: String
   var street: String
-  
   
   var address: String {
     return "\(city), \(street), \(postalCode)"
@@ -36,17 +31,15 @@ struct Station: Decodable, Identifiable, Equatable, Hashable {
 }
 
 extension Station {
-  
   static var testStation = Station(id: .init(),
-                                    city: "Kyiv",
-                                    euroA95: 53.5,
-                                    euroDP: 54,
-                                    gas: 24.99,
-                                    latitude: 50.447306,
-                                    longitude: 30.494783,
-                                    name: "EcoFill",
-                                    postalCode: "02000",
-                                    schedule: "08:00-23.00",
-                                    street: "Olesia Honchara St, 79-75")
-
+                                   city: "Kyiv",
+                                   euroA95: 53.99,
+                                   euroDP: 54.99,
+                                   gas: 24.99,
+                                   latitude: 50.447306,
+                                   longitude: 30.494783,
+                                   name: "EcoFill",
+                                   postalCode: "02000",
+                                   schedule: "08:00-23.00",
+                                   street: "Olesia Honchara St, 79-75")
 }
