0ff261f7bc43b56eb515a67063253e00c629df5a
an4lex <an4lex@gmail.com>
2024-03-10 23:48:21 +0200

    The arrangement of elements has been changed. Improved code readability.

--- EcoFill/Views/Previews/UserDataPreview.swift
+++ EcoFill/Views/Previews/UserDataPreview.swift
@@ -8,39 +8,33 @@
 import SwiftUI
 
 struct UserDataPreview: View {
-  // MARK: - Properties
+  
   @EnvironmentObject var authenticationVM: AuthenticationViewModel
   
   var body: some View {
     HStack {
       if let user = authenticationVM.currentUser {
-        VStack(alignment: .leading, spacing:10) {
+        
+        VStack(alignment: .leading, spacing: 15) {
           Text(user.fullName)
-            .font(.callout)
-            .fontWeight(.medium)
-            .fontDesign(.rounded)
-            .foregroundStyle(.defaultReversed)
+            .font(.lexendBody)
+            .foregroundStyle(.cmReversed)
           
           Text(user.email)
-            .font(.system(size: 15))
-            .fontWeight(.medium)
-            .fontDesign(.rounded)
+            .font(.lexendCallout)
             .foregroundStyle(.gray)
-        }
-        
-        Spacer()
-        
-        HStack(spacing:10) {
-          Image("city")
-            .resizable()
-            .frame(width: 25, height: 25)
-          Text(user.city)
-            .font(.callout)
-            .fontWeight(.medium)
-            .fontDesign(.rounded)
-            .foregroundStyle(.defaultReversed)
+          
+          HStack(spacing: 8) {
+            Image("city")
+              .resizable()
+              .frame(width: 28, height: 28)
+            Text(user.city)
+              .font(.lexendFootnote)
+              .foregroundStyle(.gray)
+          }
         }
       }
+      Spacer()
     }
   }
 }
