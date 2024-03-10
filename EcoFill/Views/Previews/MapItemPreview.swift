8316f294a82f085e874b4736d7e292be71bec107
an4lex <an4lex@gmail.com>
2024-03-10 23:54:57 +0200

    Improved code readability and changed the design of 'MapItem' preview.

--- EcoFill/Views/Previews/MapItemPreview.swift
+++ EcoFill/Views/Previews/MapItemPreview.swift
@@ -14,56 +14,62 @@ struct MapItemPreview: View {
   var station: Station
   
   var body: some View {
-    VStack(alignment: .leading, spacing: 13) {
-      
-      // MARK: - Coordinates
+    NavigationStack {
       VStack(alignment: .leading, spacing: 13) {
-        Text(station.name)
-          .font(.lexendTitle3)
-        
-        Text(station.address)
-          .font(.lexendFootnote)
-          .foregroundStyle(.gray)
-        
-        // MARK: - Work schedule
-        HStack {
-          Text("Work schedule:")
+        // MARK: - Coordinates
+        VStack(alignment: .leading, spacing: 13) {
+          Text(station.name)
+            .font(.lexendBody)
+          
+          Text(station.address)
             .font(.lexendFootnote)
             .foregroundStyle(.gray)
           
-          Text(station.schedule)
-            .font(.lexendCallout)
-            .foregroundStyle(.brown)
+          // MARK: - Work schedule
+          HStack {
+            Text("Work schedule:")
+              .font(.lexendFootnote)
+              .foregroundStyle(.gray)
+            
+            Text(station.schedule)
+              .font(.lexendCallout)
+              .foregroundStyle(.brown)
+          }
+        }
+        
+        // MARK: - Fuels
+        ScrollableFuelsStack(station: station)
+        
+        // MARK: - Payment methods
+        HStack(spacing: 8) {
+          Text("Pay with:")
+            .font(.lexendFootnote)
+            .foregroundStyle(.gray)
+          Image("mastercard")
+            .resizable()
+            .frame(width: 40, height: 40)
+          Image("applePay")
+            .resizable()
+            .frame(width: 40, height: 40)
+          Image("cash")
+            .resizable()
+            .frame(width: 40, height: 40)
+        }
+        
+        Button("Get directions", systemImage: "figure.walk") {
+          // action
+        }
+        .buttonStyle(ButtonModifier(pouring: .accent))
+      }
+      .padding(.horizontal, 15)
+      .padding(.bottom, 40)
+      
+      .toolbar {
+        ToolbarItem(placement: .topBarTrailing) {
+          DismissXButton()
         }
       }
-      
-      // MARK: - Fuels
-      ScrollableFuelsStack(station: station)
-      
-      // MARK: - Payment methods
-      HStack(spacing: 8) {
-        Text("Pay with:")
-          .font(.lexendFootnote)
-          .foregroundStyle(.gray)
-        Image("mastercard")
-          .resizable()
-          .frame(width: 40, height: 40)
-        Image("applePay")
-          .resizable()
-          .frame(width: 40, height: 40)
-        Image("cash")
-          .resizable()
-          .frame(width: 40, height: 40)
-      }
-      
-      Button("Get directions", systemImage: "figure.walk") {
-        // action
-      }
-      .font(.lexendBody)
-      .buttonStyle(.borderedProminent)
-      .shadow(radius: 6)
     }
-    .padding(.horizontal, 15)
   }
 }
 
