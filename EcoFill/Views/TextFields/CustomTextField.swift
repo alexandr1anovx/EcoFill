e0b6a37da59ef215362c3f0d41f43bcdd084ebba
an4lex <an4lex@gmail.com>
2024-03-11 00:05:40 +0200

    Created a custom TextField.

---
+++ EcoFill/Views/TextFields/CustomTextField.swift
@@ -0,0 +1,41 @@
+//
+//  InputView.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 21.12.2023.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+  
+  // MARK: - Properties
+  @Binding var inputData: String
+  var title: String
+  var placeholder: String
+  var isSecureField = false
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 15) {
+      Text(title)
+        .foregroundStyle(.cmReversed)
+        .font(.lexendCallout)
+      
+      if isSecureField {
+        SecureField(placeholder, text: $inputData)
+          .font(.footnote)
+      } else {
+        TextField(placeholder, text: $inputData)
+          .font(.lexendFootnote)
+          .foregroundStyle(.gray)
+      }
+      Divider()
+    }
+  }
+}
+
+#Preview {
+  CustomTextField(inputData: .constant(""),
+                  title: "Email address",
+                  placeholder: "Enter your email")
+}
