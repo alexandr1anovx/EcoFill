33512d180dcc1d35db33633f85684f60cf420bc3
an4lex <an4lex@gmail.com>
2024-03-12 23:38:10 +0200

    Added extra map controls to allow for changing the map style and displaying the list of stations.

---
+++ EcoFill/Views/MapControls/MapControls.swift
@@ -0,0 +1,46 @@
+//
+//  MapControls.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 09.03.2024.
+//
+
+import SwiftUI
+
+struct MapControls: View {
+  
+  @Binding var isPresentedList: Bool
+  @Binding var isPresentedMapStyle: Bool
+
+  var body: some View {
+    VStack(spacing: 10) {
+      ControlItem(image: .map) {
+        isPresentedMapStyle = true
+      }
+      ControlItem(image: .book) {
+        isPresentedList = true
+      }
+    }
+  }
+}
+
+struct ControlItem: View {
+  
+  var image: ImageResource
+  var action: () -> Void?
+  
+  var body: some View {
+    Button {
+      action()
+    } label: {
+      Image(image)
+        .defaultSize()
+    }
+    .buttonStyle(CustomButtonModifier(pouring: .cmSystem))
+    .shadow(radius: 5)
+  }
+}
+
+#Preview {
+  MapControls(isPresentedList: .constant(false), isPresentedMapStyle: .constant(false))
+}
