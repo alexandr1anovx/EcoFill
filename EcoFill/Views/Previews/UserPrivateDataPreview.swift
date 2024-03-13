6553e35e7b01695d153531cacbcb492e9d536eac
an4lex <an4lex@gmail.com>
2024-03-13 23:14:10 +0200

    Changed the whole design. Added ability to edit data. Improved code readability.

--- EcoFill/Views/Previews/UserPrivateDataPreview.swift
+++ EcoFill/Views/Previews/UserPrivateDataPreview.swift
@@ -6,80 +6,114 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import Firebase
 
 @MainActor
 struct UserPrivateDataPreview: View {
-  // MARK: - Properties
-  @EnvironmentObject var authViewModel: AuthViewModel
-  @FocusState private var focusedTextField: RegistrationFormTextField?
   
+  // MARK: - Properties
+  
+  @EnvironmentObject var authenticationVM: AuthenticationViewModel
   @State private var fullName: String = ""
   @State private var email: String = ""
-  @State private var isPresentedDeletion: Bool = false
+  @State private var city: String = ""
+  @State private var isPresentedEditingMode = false
+  @State private var isConfirming = false
+  @State private var isEmailVerified = false
   
-  // MARK: - body
   var body: some View {
     NavigationStack {
-      Form {
-        Section {
-          TextField("Full Name", text: $fullName)
-            .focused($focusedTextField, equals: .fullName)
-            .onSubmit { focusedTextField = .email }
-            .submitLabel(.next)
-            .textContentType(.name)
+      
+      VStack(alignment: .leading, spacing: 25) {
+        
+        // MARK: - Information Fields
+        VStack(alignment: .leading, spacing: 10) {
+          InformationRow(
+            image: .initials,
+            title: "Initials:",
+            content: fullName)
           
-          TextField("Email", text: $email)
-            .focused($focusedTextField, equals: .email)
-            .onSubmit { focusedTextField = nil }
-            .submitLabel(.done)
-            .keyboardType(.emailAddress)
-            .textInputAutocapitalization(.never)
-            .autocorrectionDisabled()
-        } header: {
-          Text("Personal data")
+          InformationRow(
+            image: .city,
+            title: "City:",
+            content: city)
+          
+          InformationRow(
+            image: isEmailVerified ? .verified : .notVerified,
+            title: "Email:",
+            content: email)
         }
         
-        Section {
-          Button("Edit", systemImage: "pencil") {
-            focusedTextField = .fullName
-          }
-          .foregroundStyle(.red)
-          
-          Button("Save changes", systemImage: "checkmark.circle.fill") {
-            
-          }
-        }
+        Divider()
         
-        Button("Delete account",
-               systemImage: "xmark.circle.fill",
-               role: .destructive,
-               action: {
-          isPresentedDeletion.toggle()
-        })
-        .foregroundStyle(.red)
-        .confirmationDialog("Are you sure?",
-                            isPresented: $isPresentedDeletion) {
-          Button("Delete", role: .destructive) {
-            authViewModel.deleteAccount()
+        // MARK: - Buttons
+        VStack(alignment: .leading, spacing: 15) {
+
+          Button("Edit information", systemImage: "pencil") {
+            isPresentedEditingMode = true
           }
-        } message: {
-          Text("All your data will be deleted.")
+          .buttonStyle(CustomButtonModifier(pouring: .cmBlack))
+          
+          Button("Delete account", systemImage: "xmark.circle.fill") {
+            isConfirming = true
+          }
+          .buttonStyle(CustomButtonModifier(pouring: .red))
+          
+          .confirmationDialog("", isPresented: $isConfirming) {
+            Button("Delete", role: .destructive) {
+              authenticationVM.deleteUser()
+            }
+          } message: {
+            Text("All your data will be deleted.")
+          }
         }
+        .shadow(radius: 5)
+        
+        Spacer()
+      }
+      .padding(.top, 20)
+      .padding(.horizontal)
+      .navigationTitle("Settings")
+      .navigationBarTitleDisplayMode(.inline)
+      
+      .sheet(isPresented: $isPresentedEditingMode) {
+        EditingScreen()
+          .presentationDetents([.large])
+          .presentationCornerRadius(20)
+          .presentationDragIndicator(.visible)
       }
       
-      /// Show an alert, the type of which depends of the Form error.
-//      .alert(item: $authViewModel.alertItem) { alertItem in
-//        Alert(title: alertItem.title,
-//              message: alertItem.message,
-//              dismissButton: alertItem.dismissButton)
-//      }
+      .alert(item: $authenticationVM.alertItem) { alertItem in
+        Alert(
+          title: alertItem.title,
+          message: alertItem.message,
+          dismissButton: alertItem.dismissButton)
+      }
+      
+      .onAppear {
+        // Load user data.
+        if let user = authenticationVM.currentUser {
+          fullName = user.fullName
+          email = user.email
+          city = user.city
+        }
+        // Verification for email confirmation by user.
+        checkEmailVerification()
+      }
+    }
+  }
+  
+  func checkEmailVerification() {
+    if let userSession = authenticationVM.userSession {
+      isEmailVerified = userSession.isEmailVerified
     }
   }
 }
 
 #Preview {
   UserPrivateDataPreview()
-    .environmentObject(AuthViewModel())
+    .environmentObject(AuthenticationViewModel())
 }
 
-//extension
+
