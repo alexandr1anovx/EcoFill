7ec16d76afcde89c68b6e39e17d0b1fcc6d47369
an4lex <an4lex@gmail.com>
2024-03-13 23:56:27 +0200

    Created an extension for 'Font' class. Added custom fonts for medium size.

---
+++ EcoFill/Helpers/Fonts/Fonts.swift
@@ -0,0 +1,37 @@
+//
+//  Fonts.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 20.02.2024.
+//
+
+import SwiftUI
+
+extension Font {
+  
+  // Medium Size
+  
+  public static var lexendTitle3: Font {
+    return Font.custom("LexendDeca-Regular", size: 19)
+  }
+  
+  public static var lexendHeadline: Font {
+    return Font.custom("LexendDeca-Semibold", size: 16)
+  }
+  
+  public static var lexendBody: Font {
+    return Font.custom("LexendDeca-Regular", size: 16)
+  }
+  
+  public static var lexendCallout: Font {
+    return Font.custom("LexendDeca-Regular", size: 15)
+  }
+  
+  public static var lexendFootnote: Font {
+    return Font.custom("LexendDeca-Regular", size: 14)
+  }
+  
+  public static var lexendCaption1: Font {
+    return Font.custom("LexendDeca-Regular", size: 11)
+  }
+}
