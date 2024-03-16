2f0e920b04e1a7218cd61cd31d1a1e0fe3aa587d
an4lex <an4lex@gmail.com>
2024-03-16 16:32:36 +0200

    Сreated a row with an image, title, and the specified content.

---
+++ EcoFill/Views/InformationRow/InformationRow.swift
@@ -0,0 +1,34 @@
+//
+//  InformationRow.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 12.03.2024.
+//
+
+import SwiftUI
+
+struct InformationRow: View {
+  
+  let image: ImageResource
+  let title: String
+  let content: String?
+  
+  var body: some View {
+    
+    HStack(spacing: 8) {
+      Image(image)
+        .defaultSize()
+      Text(title)
+        .font(.lexendFootnote)
+        .foregroundStyle(.gray)
+      Text(content ?? "")
+        .font(.lexendFootnote)
+        .foregroundStyle(.cmReversed)
+    }
+  }
+}
+
+#Preview {
+  InformationRow(
+    image: .initials, title: "Initials:", content: "Tim Cook")
+}
