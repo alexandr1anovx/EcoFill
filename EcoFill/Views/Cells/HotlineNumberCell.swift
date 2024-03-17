06718e30c15def0415a63148b705450b8f0750f2
an4lex <an4lex@gmail.com>
2024-03-17 22:19:46 +0200

    The cell containing hotline phone number has been created.

---
+++ EcoFill/Views/Cells/HotlineNumberCell.swift
@@ -0,0 +1,24 @@
+//
+//  HotlineNumberCell.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 28.02.2024.
+//
+
+import SwiftUI
+
+struct HotlineNumberCell: View {
+  
+  var phoneNumber: String
+  
+  var body: some View {
+    Button(phoneNumber, systemImage: "phone") {
+      // make phone call
+    }
+    .buttonStyle(CustomButtonModifier(pouring: .accent))
+  }
+}
+
+#Preview {
+  HotlineNumberCell(phoneNumber: "(050)-660-02-30")
+}
