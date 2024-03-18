38876a0f08006d804c006d0569d3d0bf31dce722
an4lex <an4lex@gmail.com>
2024-03-19 00:01:45 +0200

    Improved the code readability.

--- EcoFill/Screens/ProfileScreen.swift
+++ EcoFill/Screens/ProfileScreen.swift
@@ -11,48 +11,49 @@ struct ProfileScreen: View {
   
   // MARK: - Properties
   @EnvironmentObject var authenticationVM: AuthenticationViewModel
-  @State private var isPresentedLogOutConfirmation = false
+  @State private var isConfirming = false
   
   // MARK: - body
   var body: some View {
     NavigationStack {
       UserDataPreview()
-        .padding(.vertical,30)
-        .padding(.horizontal,23)
+        .padding(20)
       
       List {
         AppearanceChanger()
         
-        Button("Log Out", systemImage: "person.crop.circle.badge.xmark") {
-          isPresentedLogOutConfirmation = true
+        Button {
+          isConfirming = true
+        } label: {
+          HStack(spacing: 20) {
+            Image(.xmark)
+              .defaultSize()
+            Text("Sign Out")
+              .font(.lexendBody)
+              .foregroundStyle(.red)
+          }
         }
-        .font(.custom("LexendDeca-Regular", size: 16))
-        .foregroundStyle(.red)
-        
-        .confirmationDialog("", isPresented: $isPresentedLogOutConfirmation) {
-          Button("Log Out", role: .destructive) {
+        .confirmationDialog("Sign Out", isPresented: $isConfirming) {
+          Button("Sign Out", role: .destructive) {
             authenticationVM.signOut()
           }
-        } message: {
-          Text("Are you sure to log out?")
         }
+        
       }
       .listStyle(.insetGrouped)
       
       .toolbar {
         ToolbarItem(placement: .topBarLeading) {
-          Image("logo")
-            .resizable()
-            .frame(width:32, height:32)
+          Image(.logo)
+            .navBarSize()
         }
         
         ToolbarItem(placement: .topBarTrailing) {
           NavigationLink {
             UserPrivateDataPreview()
           } label: {
-            Image(systemName: "pencil.and.list.clipboard")
-              .imageScale(.large)
-              .tint(.accent)
+            Image(.edit)
+              .navBarSize()
           }
         }
       }
@@ -66,3 +67,4 @@ struct ProfileScreen: View {
   ProfileScreen()
     .environmentObject(AuthenticationViewModel())
 }
+
