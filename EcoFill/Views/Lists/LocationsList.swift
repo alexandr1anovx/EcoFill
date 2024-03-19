b9f2824e22159bae939c71ae6c771c80c0ef9433
an4lex <an4lex@gmail.com>
2024-03-19 23:59:25 +0200

    Added a list with stations location.

---
+++ EcoFill/Views/Lists/LocationsList.swift
@@ -0,0 +1,61 @@
+//
+//  TestPicker.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 12.02.2024.
+//
+
+import SwiftUI
+import UIKit
+
+struct LocationsList: View {
+  
+  // MARK: - Properties
+  @EnvironmentObject var authenticationVM: AuthenticationViewModel
+  @EnvironmentObject var firestoreVM: FirestoreViewModel
+  @State private var selectedIndex = 0
+
+  let cities = ["Kyiv", "Odesa", "Mykolaiv"]
+  
+  var filteredStations: [Station] {
+    firestoreVM.stations.filter {
+      $0.city == cities[selectedIndex]
+    }
+  }
+  
+  var body: some View {
+    VStack {
+      Picker("", selection: $selectedIndex) {
+        ForEach(cities.indices, id: \.self) { index in
+          Text(cities[index])
+        }
+      }
+      .pickerStyle(.segmented)
+      .colorMultiply(.brown)
+      .padding(.top,10)
+      .padding(15)
+      
+      List(filteredStations) { station in
+        StationCoordinatesCell(station: station)
+      }
+      .listStyle(.plain)
+      .listRowSpacing(15)
+    }
+    .onAppear {
+      firestoreVM.fetchStations()
+      
+      // Setup an index for selected city.
+      if let selectedCity = authenticationVM.currentUser?.city {
+        if let index = cities.firstIndex(of: selectedCity) {
+          selectedIndex = index
+        }
+      }
+    }
+  }
+}
+
+#Preview {
+  LocationsList()
+    .environmentObject(FirestoreViewModel())
+    .environmentObject(AuthenticationViewModel())
+}
