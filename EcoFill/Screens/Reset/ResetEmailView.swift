ed3cf611cc3c0ffaa09cd324a1b6a8d9c269b038
an4lex <an4lex@gmail.com>
2024-03-23 02:52:08 +0200

    Created a view with ability to reset the email address.

---
+++ EcoFill/Screens/Reset/ResetEmailView.swift
@@ -0,0 +1,91 @@
+//
+//  ChangeEmailScreen.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 22.03.2024.
+//
+
+import SwiftUI
+
+import SwiftUI
+import Firebase
+import FirebaseFirestoreSwift
+
+struct ResetEmailView: View {
+  
+  // MARK: - Properties
+  @EnvironmentObject var authenticationVM: AuthenticationViewModel
+  
+  @State private var email: String = ""
+  @State private var currentPassword: String = ""
+  
+  @FocusState private var fieldContent: TextFieldContent?
+  
+  var body: some View {
+    NavigationStack {
+      
+      VStack(alignment: .leading, spacing: 15) {
+        
+        // MARK: - Text Fields
+        
+        CustomTextField(inputData: $email,
+                        title: "New email address",
+                        placeholder: "example@gmail.com")
+        .focused($fieldContent, equals: .email)
+        .onSubmit { fieldContent = .password }
+        .submitLabel(.next)
+        .keyboardType(.emailAddress)
+        .textInputAutocapitalization(.never)
+        .autocorrectionDisabled(true)
+        
+        CustomTextField(inputData: $currentPassword,
+                        title: "Current password",
+                        placeholder: "Repeat your current password.",
+                        isSecureField: true)
+        .focused($fieldContent, equals: .password)
+        .onSubmit { fieldContent = .confirmPassword }
+        .submitLabel(.next)
+        
+        UniversalButton(image: .checkmark, title: "Reset", color: .white, spacing: 10) {
+          Task {
+            await authenticationVM.updateEmail(
+              withEmail: email, password: currentPassword
+            )
+          }
+          return fieldContent = nil
+        }
+        .buttonStyle(CustomButtonModifier(pouring: .cmBlack))
+        .opacity(isValidForm ? 1.0 : 0.0)
+        
+        Spacer()
+      }
+      .padding(.horizontal, 15)
+      .padding(.top, 30)
+      .toolbar {
+        ToolbarItem(placement: .topBarLeading) { DismissXButton() }
+      }
+      // Enable the keyboard.
+      .onAppear { fieldContent = .fullName }
+      
+      .alert(item: $authenticationVM.alertItem) { alertItem in
+        Alert(title: alertItem.title,
+              message: alertItem.message,
+              dismissButton: alertItem.dismissButton)
+      }
+    }
+  }
+}
+
+// MARK: - Extensions
+
+extension ResetEmailView: AuthenticationForm {
+  var isValidForm: Bool {
+    return email.isValidEmail && currentPassword.count > 5
+  }
+}
+
+#Preview {
+  ResetEmailView()
+    .environmentObject(AuthenticationViewModel())
+}
+
