1d24f1bac43d095fb3627dc771a32c56974c913a
an4lex <an4lex@gmail.com>
2024-03-26 23:55:51 +0200

    The 'Sign-In' button has been added.

---
+++ EcoFill/Views/Buttons/SignInBtn.swift
@@ -0,0 +1,32 @@
+//
+//  SignInBtn.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 23.03.2024.
+//
+
+import SwiftUI
+
+struct SignInBtn: View {
+  var action: () -> Void
+  
+  var body: some View {
+    Button {
+      action()
+    } label: {
+      HStack {
+        Image(.userSignIn)
+          .defaultSize()
+        Text("Sign In")
+          .font(.lexendFootnote)
+          .foregroundStyle(.white)
+      }
+    }
+    .buttonStyle(CustomButtonModifier(pouring: .accent))
+    .shadow(radius: 5)
+  }
+}
+
+#Preview {
+  SignInBtn(action: {})
+}
