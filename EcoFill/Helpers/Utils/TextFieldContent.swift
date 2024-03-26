f85990495973b5a49e3d13e15c118d2f5f82112c
an4lex <an4lex@gmail.com>
2024-03-27 01:38:53 +0200

    Created a separate enum for the text field content.

---
+++ EcoFill/Helpers/Utils/TextFieldContent.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldContent.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 23.03.2024.
+//
+
+import Foundation
+
+enum TextFieldContent: String {
+  case fullName
+  case email
+  case password
+  case confirmPassword
+  
+  case newPassword
+}
