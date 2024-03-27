fbb4851d68be8dc980a1e303087ea6b56b40164f
an4lex <an4lex@gmail.com>
2024-03-27 12:37:46 +0200

    Custom 'Sign Up' button has been created.

---
+++ EcoFill/Views/Buttons/SignUpBtn.swift
@@ -0,0 +1,32 @@
+//
+//  SignUpBtn.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 23.03.2024.
+//
+
+import SwiftUI
+
+struct SignUpBtn: View {
+  var action: () -> Void
+  
+  var body: some View {
+    Button {
+      action()
+    } label: {
+      HStack {
+        Image(.userSignUp)
+          .defaultSize()
+        Text("Sign Up")
+          .font(.lexendFootnote)
+          .foregroundStyle(.white)
+      }
+    }
+    .buttonStyle(CustomButtonModifier(pouring: .orange))
+    .shadow(radius: 5)
+  }
+}
+
+#Preview {
+  SignUpBtn(action: {})
+}
