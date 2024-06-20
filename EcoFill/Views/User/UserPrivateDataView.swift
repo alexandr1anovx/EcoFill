b2238ac31d5d5113d5f58f2f57fb09b145423adc
an4lex <an4lex@gmail.com>
2024-06-20 15:30:03 +0300

    Removed the "changePassword" option. Some properties were renamed.

--- EcoFill/Views/User/UserPrivateDataView.swift
+++ EcoFill/Views/User/UserPrivateDataView.swift
@@ -12,61 +12,53 @@ import Firebase
 @MainActor
 struct UserPrivateDataView: View {
   
-  // MARK: - Properties
-  
+  // MARK: - properties
   @EnvironmentObject var authenticationVM: AuthenticationViewModel
-  
   @State private var fullName: String = ""
   @State private var email: String = ""
   @State private var city: String = ""
   @State private var currentPassword: String = ""
-  
-  @State private var isPresentedEmailChange = false
-  @State private var isPresentedPasswordChange = false
-  
+  @State private var isShownResetEmail = false
+  @State private var isShownResetPassword = false
   @State private var isConfirming = false
-  @State private var isEmailVerified = false
+  @State private var isVerifiedEmail = false
   
   var body: some View {
     NavigationStack {
-      
       VStack(alignment: .leading, spacing: 20) {
         
-        // MARK: - Information Fields
+        // MARK: - information fields
         VStack(alignment: .leading, spacing: 10) {
+
           InformationRow(
-            image: .initials,
-            title: "Initials:",
+            img: .initials,
+            text: "Initials:",
             content: fullName)
           
           InformationRow(
-            image: .building,
-            title: "City:",
+            img: .location,
+            text: "City:",
             content: city)
           
           VStack(alignment: .leading, spacing: 10) {
             InformationRow(
-              image: isEmailVerified ? .verified : .notVerified,
-              title: "Email:",
+              img: isVerifiedEmail ? .verified : .notVerified,
+              text: "Email:",
               content: email)
             
-            Text(isEmailVerified ?
-                 "Email is vefiried." : "Email is unverified. Confirm the link by email and re-login to your account.")
-            .font(.lexendCaption1)
-            .foregroundStyle(isEmailVerified ? .accent : .red)
+            Text(isVerifiedEmail ? "Email is vefiried." : "Email is unverified. Confirm the link by email and re-login to your account.")
+            .font(.lexendCaption)
+            .foregroundStyle(isVerifiedEmail ? .accent : .red)
           }
         }
         
         Divider()
         
-        // MARK: - Buttons
+        // MARK: - buttons
+        
         VStack(alignment: .leading, spacing: 15) {
-          
-          ChangeBtn(title: "email", img: .email) {
-            isPresentedEmailChange = true
-          }
-          ChangeBtn(title: "password", img: .password) {
-            isPresentedPasswordChange = true
+          ChangeDataBtn(data: "email", img: .mail) {
+            isShownResetEmail = true
           }
           
           DeleteAccountBtn { isConfirming = true }
@@ -89,19 +81,16 @@ struct UserPrivateDataView: View {
       .navigationBarTitleDisplayMode(.inline)
       
       // MARK: - Sheets
-      
-      .sheet(isPresented: $isPresentedEmailChange) {
+      .sheet(isPresented: $isShownResetEmail) {
         ResetEmailView()
           .presentationCornerRadius(20)
       }
-      
-      .sheet(isPresented: $isPresentedPasswordChange) {
+      .sheet(isPresented: $isShownResetPassword) {
         ResetPasswordView()
           .presentationCornerRadius(20)
       }
       
       // MARK: - Alert
-      
       .alert(item: $authenticationVM.alertItem) { alertItem in
         Alert(
           title: alertItem.title,
@@ -125,7 +114,7 @@ struct UserPrivateDataView: View {
   
   private func checkEmailVerification() {
     if let user = authenticationVM.userSession {
-      isEmailVerified = user.isEmailVerified
+      isVerifiedEmail = user.isEmailVerified
     }
   }
 }
