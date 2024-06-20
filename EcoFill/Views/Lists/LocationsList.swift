e14e1569b302dc0b82393c26dc20f4776f44a424
an4lex <an4lex@gmail.com>
2024-06-20 15:58:41 +0300

    Added feature to show the route of selected station from list. Synchronized with MapItemView.

--- EcoFill/Views/Lists/LocationsList.swift
+++ EcoFill/Views/Lists/LocationsList.swift
@@ -6,21 +6,21 @@
 //
 
 import SwiftUI
-import UIKit
 
 struct LocationsList: View {
   
-  // MARK: - Properties
+  // MARK: - properties
   @EnvironmentObject var authenticationVM: AuthenticationViewModel
   @EnvironmentObject var firestoreVM: FirestoreViewModel
   @State private var selectedIndex = 0
+  @Binding var selectedStation: Station?
+  @Binding var isShownStationDetails: Bool
+  @Binding var isShownRoute: Bool
   
   let cities = ["Kyiv", "Odesa", "Mykolaiv"]
   
   var filteredStations: [Station] {
-    firestoreVM.stations.filter {
-      $0.city == cities[selectedIndex]
-    }
+    firestoreVM.stations.filter { $0.city == cities[selectedIndex] }
   }
   
   var body: some View {
@@ -35,26 +35,34 @@ struct LocationsList: View {
       .padding(15)
       
       List(filteredStations) { station in
-        StationCoordinatesCell(station: station)
+        
+        let isShownRouteCondition = selectedStation == station && isShownRoute
+        
+        StationCell(station: station, isShownRoute: isShownRouteCondition) {
+          
+          isShownStationDetails = false
+          
+          if isShownRouteCondition {
+            selectedStation = nil
+            isShownRoute = false
+            
+          } else {
+            selectedStation = station
+            isShownRoute = true
+            
+          }
+        }
       }
       .listStyle(.plain)
       .listRowSpacing(15)
     }
+    
     .onAppear {
-      firestoreVM.fetchStations()
-      
-      // Setup an index for selected city.
       if let selectedCity = authenticationVM.currentUser?.city,
          let index = cities.firstIndex(of: selectedCity) {
         selectedIndex = index
       }
     }
+    
   }
-}
-
-
-#Preview {
-  LocationsList()
-    .environmentObject(FirestoreViewModel())
-    .environmentObject(AuthenticationViewModel())
 }
