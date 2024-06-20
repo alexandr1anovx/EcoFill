0719feb6446faaa0fe0cbf0588cd499d7302ed9a
an4lex <an4lex@gmail.com>
2024-06-20 18:25:38 +0300

    Minor UI updates.

--- EcoFill/Views/Buttons/ResetBtn.swift
+++ EcoFill/Views/Buttons/ResetBtn.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ResetBtn: View {
+  var img: ImageResource
+  var data: String
   var action: () -> Void
   
   var body: some View {
@@ -15,15 +17,34 @@ struct ResetBtn: View {
       action()
     } label: {
       HStack {
-        Image(.cmCheckmark)
-          .resizable()
-          .frame(width: 20, height: 20)
-        Text("Reset")
-          .font(.lexendCallout)
+        Image(img)
+          .defaultSize()
+        Text("Reset \(data)")
+          .font(.lexendFootnote)
           .foregroundStyle(.white)
       }
     }
     .buttonStyle(CustomButtonModifier(pouring: .cmBlack))
     .shadow(radius: 5)
+    
+    
+    
+    
+    
+    
+//    Button {
+//      action()
+//    } label: {
+//      HStack {
+//        Image(.cmCheckmark)
+//          .resizable()
+//          .frame(width: 20, height: 20)
+//        Text("Reset")
+//          .font(.lexendCallout)
+//          .foregroundStyle(.white)
+//      }
+//    }
+//    .buttonStyle(CustomButtonModifier(pouring: .cmBlack))
+//    .shadow(radius: 5)
   }
 }
