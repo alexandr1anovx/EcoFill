85e1f834daac851b5ebab837b40b22fbc0f1d085
andr1anovv <an4lex@gmail.com>
2024-08-04 03:41:45 +0300

    Alert content has been changed

--- EcoFill/Helpers/Alerts/Alert.swift
+++ EcoFill/Helpers/Alerts/Alert.swift
@@ -10,35 +10,54 @@ import SwiftUI
 struct AlertItem: Identifiable {
   let id = UUID()
   let title: Text
-  let message: Text
+  let message: Text?
   let dismissButton: Alert.Button
+  
+  // Custom initializer with default value for message
+  init(title: Text, message: Text? = nil, dismissButton: Alert.Button) {
+    self.title = title
+    self.message = message
+    self.dismissButton = dismissButton
+  }
 }
 
 struct RegistrationAlertContext {
   
-  // There is no account with entered data.
-  static let nonExistentUser = AlertItem(title: Text("Sign In Error"),
-                                         message: Text("There is no account with entered data."),
-                                         dismissButton: .default(Text("OK")))
+  static let userDoesNotExists = AlertItem(
+    title: Text("Sign In Error"),
+    message: Text("User with entered data does not exist"),
+    dismissButton: .default(Text("OK"))
+  )
   
-  // An account with the entered data already exists.
-  static let existingUser = AlertItem(title: Text("Sign Up Error"),
-                                      message: Text("An account with the entered data already exists."),
-                                      dismissButton: .default(Text("OK")))
+  static let userExists = AlertItem(
+    title: Text("Sign Up Error"),
+    message: Text("User with entered data already exists"),
+    dismissButton: .default(Text("OK"))
+  )
 }
 
 struct ProfileAlertContext {
   
-  // Account deleted successfully.
-  static let accountDeleted = AlertItem(title: Text("Account deleted"),
-                                        message: Text("All your data has been successfully erased."),
-                                        dismissButton: .default(Text("OK")))
+  static let successfullAccountDeletion = AlertItem(
+    title: Text("Account successfully deleted"),
+    dismissButton: .default(Text("OK"))
+  )
   
-  static let emailUpdateFailed = AlertItem(title: Text("Email Update Failed"),
-                                             message: Text("This email address is already taken."),
-                                             dismissButton: .default(Text("OK")))
+  static let unsuccessfullAccountDeletion = AlertItem(
+    title: Text("Unsuccessfull Account Deletion"),
+    message: Text("Something went wrong."),
+    dismissButton: .default(Text("OK"))
+  )
   
-  static let emailVerificationSent = AlertItem(title: Text("Check Your Email"),
-                                              message: Text("The confirmation link has been sent to the specified email."),
-                                              dismissButton: .default(Text("OK")))
+  static let unsuccessfullEmailUpdate = AlertItem(
+    title: Text("Unsuccessfull Email Update"),
+    message: Text("This email address is already taken."),
+    dismissButton: .default(Text("OK"))
+  )
+  
+  static let confirmationLinkSent = AlertItem(
+    title: Text("Confirmation Link Sent"),
+    message: Text("The confirmation link has been sent to the specified email."),
+    dismissButton: .default(Text("OK"))
+  )
 }
