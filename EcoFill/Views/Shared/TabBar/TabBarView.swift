32e8a1541252877e7b27013eb48b90de6735873e
andr1anovv <an4lex@gmail.com>
2024-10-19 22:50:08 +0300

    Fixed a bag where tab bar was displayed on every screen

--- EcoFill/Views/Shared/TabBar/TabBarView.swift
+++ EcoFill/Views/Shared/TabBar/TabBarView.swift
@@ -10,37 +10,43 @@ import SwiftUI
 struct TabBarView: View {
     
     @State private var selectedTab = TabBarItem.home
+    @State private var isShownTabBar: Bool = true
+    
     init() { UITabBar.appearance().isHidden = true }
     
     var body: some View {
         ZStack(alignment: .bottom) {
             TabView(selection: $selectedTab) {
-                HomeScreen()
+                HomeScreen(isShownTabBar: $isShownTabBar)
                     .tag(TabBarItem.home)
-                MapScreen()
+                MapScreen(isShownTabBar: $isShownTabBar)
                     .tag(TabBarItem.map)
-                ProfileScreen()
+                ProfileScreen(isShownTabBar: $isShownTabBar)
                     .tag(TabBarItem.profile)
             }
-            HStack {
-                ForEach(TabBarItem.allCases, id: \.self) { tab in
-                    TabBarButton(
-                        title: tab.title,
-                        image: tab.iconName,
-                        tab: tab,
-                        selectedTab: $selectedTab
-                    )
-                    if tab != TabBarItem.allCases.last {
-                        Spacer()
+            
+            if isShownTabBar {
+                HStack {
+                    ForEach(TabBarItem.allCases, id: \.self) { tab in
+                        TabBarButton(
+                            title: tab.title,
+                            image: tab.iconName,
+                            tab: tab,
+                            selectedTab: $selectedTab
+                        )
+                        if tab != TabBarItem.allCases.last {
+                            Spacer()
+                        }
                     }
                 }
+                .padding(.vertical, 11)
+                .padding(.horizontal)
+                .background(.tabBarBackground)
+                .clipShape(.capsule)
+                .padding(.horizontal, 25)
+                .shadow(radius: 8)
             }
-            .padding(.vertical, 11)
-            .padding(.horizontal)
-            .background(.primaryTabBar)
-            .clipShape(.capsule)
-            .padding(.horizontal, 25)
-            .shadow(radius: 6)
         }
     }
 }
+
