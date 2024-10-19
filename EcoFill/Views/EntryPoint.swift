039987e2df274661ecb107b0ba39db825b98979b
andr1anovv <an4lex@gmail.com>
2024-10-19 22:57:07 +0300

    Moved the "getStations" method to main App file

--- EcoFill/Views/EntryPoint.swift
+++ EcoFill/Views/EntryPoint.swift
@@ -4,13 +4,12 @@ struct EntryPoint: View {
     
     @State private var isShownContent: Bool = false
     @EnvironmentObject var userVM: UserViewModel
-    @EnvironmentObject var stationVM: StationViewModel
     
     var body: some View {
         Group {
             if userVM.userSession != nil {
                 if isShownContent {
-                    TabBarView()
+                    TabBarView()   
                 } else {
                     LaunchView()
                 }
@@ -19,7 +18,6 @@ struct EntryPoint: View {
             }
         }
         .onAppear {
-            stationVM.getStations()
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                 withAnimation(.spring(duration: 1)) {
                     isShownContent.toggle()
