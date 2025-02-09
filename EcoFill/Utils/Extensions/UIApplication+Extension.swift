a54a503a66f19fd8ab86a2139a354f588c9abc0b
Alexander Andrianov <an4lex@gmail.com>
2025-02-09 23:32:04 +0200

    The hiding keyboard method has been renamed

--- EcoFill/Utils/Extensions/UIApplication+Extension.swift
+++ EcoFill/Utils/Extensions/UIApplication+Extension.swift
@@ -9,10 +9,12 @@ import UIKit
 
 // Disables the keyboard when the user taps on any part of the screen.
 extension UIApplication {
-  func endEditing() {
-    sendAction(#selector(UIResponder.resignFirstResponder),
-               to: nil,
-               from: nil,
-               for: nil)
+  func hideKeyboard() {
+    sendAction(
+      #selector(UIResponder.resignFirstResponder),
+      to: nil,
+      from: nil,
+      for: nil
+    )
   }
 }
