c285e36c5b990d8098ee0deae3333f095f702135
Alexander Andrianov <an4lex@gmail.com>
2025-03-02 01:48:41 +0200

    A new text field for entering large messages has been applied.

--- EcoFill/Views/Screens/Support/SupportScreen.swift
+++ EcoFill/Views/Screens/Support/SupportScreen.swift
@@ -8,17 +8,15 @@ struct SupportScreen: View {
   @EnvironmentObject var userVM: UserViewModel
   
   private var isMessageCorrect: Bool {
-    message.count > 10
+    message.count > 10 && message.count < 500
   }
   
   var body: some View {
     ZStack {
       Color.primaryBackground.ignoresSafeArea(.all)
-      VStack(spacing: 20) {
+      VStack(spacing:0) {
         textFields
-        guideLabel.padding(.horizontal)
         sendMessageButton
-        Spacer()
       }
       .navigationTitle("Support")
       .navigationBarTitleDisplayMode(.inline)
@@ -36,37 +34,31 @@ struct SupportScreen: View {
   
   private var textFields: some View {
     List {
-      CSTextField(
-        icon: .envelope,
-        hint: "Your email address",
-        inputData: $emailAddress
-      )
-      .disabled(true)
-      
-      CSTextField(
-        icon: .message,
-        hint: "At least 10 characters.",
-        inputData: $message
-      )
-      .submitLabel(.done)
+      Section {
+        CSTextField(
+          icon: .envelope,
+          hint: "Your email address",
+          inputData: $emailAddress
+        )
+        .disabled(true)
+        
+        TextFieldForMessage("At least 10 characters.", $message)
+          .submitLabel(.done)
+      } header: {
+        Text("Send your feedback")
+      } footer: {
+        Text("Our support team is here to help! Please use polite and respectful language in all communications.")
+      }
     }
-    .listStyle(.sidebar)
-    .frame(height: 140)
+    .listStyle(.insetGrouped)
     .scrollContentBackground(.hidden)
     .scrollIndicators(.hidden)
     .scrollDisabled(true)
     .shadow(radius: 2)
   }
   
-  private var guideLabel: some View {
-    Text("Our support team is here to help! Please use polite and respectful language in all communications.")
-      .font(.caption2)
-      .foregroundStyle(.gray)
-      .multilineTextAlignment(.leading)
-  }
-  
   private var sendMessageButton: some View {
-    CSButton(title: "Send Message", color: .accent) {
+    CSButton("Send Message", color: .accent) {
       message = ""
       isShownAlert.toggle()
     }
