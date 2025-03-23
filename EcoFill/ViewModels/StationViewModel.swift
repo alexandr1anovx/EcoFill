4c1e6dfee5e54d62031b2e29bd366491a82d652f
Alexander Andrianov <an4lex@gmail.com>
2025-03-23 02:42:24 +0200

    Added a method for selecting different types of transport on the map.

--- EcoFill/ViewModels/StationViewModel.swift
+++ EcoFill/ViewModels/StationViewModel.swift
@@ -7,10 +7,11 @@ final class StationViewModel: ObservableObject {
   
   @Published var stations: [Station] = []
   @Published var selectedStation: Station?
+  @Published var selectedTransportType: MKDirectionsTransportType = .automobile
   @Published var route: MKRoute?
   @Published var isShownRoute = false
-  @Published var isShownDetail = false
-  @Published var isShownList = false
+  @Published var isShownStationDataSheet = false
+  @Published var isShownStationList = false
   
   private let locationManager = LocationManager.shared
   
@@ -55,18 +56,14 @@ final class StationViewModel: ObservableObject {
   
   func getRoute(to station: Station?) async {
     route = nil
-    guard let station else { return } // check if the station exist
+    guard let station else { return }
     guard let userLocation = locationManager.manager.location else { return }
-    
     let userCoordinate = userLocation.coordinate
     let userPlacemark = MKPlacemark(coordinate: userCoordinate)
-    
     let stationCoordinate = station.coordinate
     let stationPlacemark = MKPlacemark(coordinate: stationCoordinate)
-    
     let source = MKMapItem(placemark: userPlacemark)
     let destination = MKMapItem(placemark: stationPlacemark)
-    
     self.route = await calculateDirections(from: source, to: destination)
   }
   
@@ -83,7 +80,7 @@ final class StationViewModel: ObservableObject {
   // MARK: - Private Methods
   private func calculateDirections(from: MKMapItem, to: MKMapItem) async -> MKRoute? {
     let request = MKDirections.Request()
-    request.transportType = .automobile
+    request.transportType = selectedTransportType
     request.source = from
     request.destination = to
     
