ca7f234ae3a24ae8b652dcd8ccea31e11878bdec
Alexander Andrianov <an4lex@gmail.com>
2025-05-18 01:50:02 +0300

    Added a separate struct for the Mock data.

---
+++ EcoFill/Models/MockData.swift
@@ -0,0 +1,29 @@
+//
+//  MockData.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 14.05.2025.
+//
+
+
+struct MockData {
+  static let user = User(
+    id: "1",
+    fullName: "Name Surname",
+    email: "example@gmail.com",
+    city: "London",
+    points: 10
+  )
+  
+  static let station = Station(
+    id: "2",
+    city: "Mykolaiv",
+    euroA95: 40.0,
+    euroDP: 30.0,
+    gas: 20.0,
+    latitude: 46.968629,
+    longitude: 31.957523,
+    schedule: "08:00-20:00",
+    street: "Unknown street"
+  )
+}
