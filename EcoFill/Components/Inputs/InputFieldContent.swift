6fc6b92a5d67e2df491826eb203ac1bd9344fb86
Alexander Andrianov <an4lex@gmail.com>
2025-05-18 02:15:42 +0300

    An enumeration with the input field content was separated into one single file.

---
+++ EcoFill/Components/Inputs/InputFieldContent.swift
@@ -0,0 +1,14 @@
+//
+//  InputFieldContent.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 13.05.2025.
+//
+
+enum InputFieldContent {
+  case fullName
+  case emailAddress
+  case password
+  case confirmPassword
+  case supportMessage
+}
