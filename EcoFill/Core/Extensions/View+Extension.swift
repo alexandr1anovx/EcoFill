7576f45ce2b8eaba6b1d44c940dcc50a6e81092a
Alexander Andrianov <an4lex@gmail.com>
2025-05-19 20:52:04 +0300

    Removed the method for configuring the segmented control UI. Improved the custom list setup method.

--- EcoFill/Core/Extensions/View+Extension.swift
+++ EcoFill/Core/Extensions/View+Extension.swift
@@ -5,20 +5,20 @@
 //  Created by Alexander Andrianov on 14.05.2025.
 //
 
-import SwiftUICore
-import UIKit
+import SwiftUI
 
 extension View {
   
   // MARK: Custom List Setup
   
-  func customListSetup(
-    height: CGFloat? = nil,
+  func customListStyle(
     rowHeight: CGFloat = 0,
     rowSpacing: CGFloat = 0,
     sectionSpacing: CGFloat = 0,
-    shadow: CGFloat = 0,
-    scrollDisabled: Bool = false
+    scrollDisabled: Bool = false,
+    indicators: ScrollIndicatorVisibility = .hidden,
+    height: CGFloat? = nil,
+    shadow: CGFloat = 0
   ) -> some View {
     self
       .listStyle(.insetGrouped)
@@ -27,22 +27,8 @@ extension View {
       .listSectionSpacing(sectionSpacing)
       .scrollContentBackground(.hidden)
       .scrollDisabled(scrollDisabled)
-      .scrollIndicators(.hidden)
+      .scrollIndicators(indicators)
       .frame(height: height)
       .shadow(radius: shadow)
-  }
-  
-  // MARK: Segmented Control UI Setup
-  
-  func setupSegmentedControlUI() {
-    let appearance = UISegmentedControl.appearance()
-    appearance.selectedSegmentTintColor = .buttonBackground
-    appearance.setTitleTextAttributes(
-      [.foregroundColor: UIColor.primaryText], for: .selected
-    )
-    appearance.setTitleTextAttributes(
-      [.foregroundColor: UIColor.label], for: .normal
-    )
-    appearance.backgroundColor = .systemBackground
   }
 }
