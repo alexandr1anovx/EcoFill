6de7253051a8f7997d8f27933740a44dcfac449e
Alexander Andrianov <an4lex@gmail.com>
2025-05-20 23:27:45 +0300

    Added a station sorting feature to the view model.

--- EcoFill/Features/Map/ViewModels/StationViewModel.swift
+++ EcoFill/Features/Map/ViewModels/StationViewModel.swift
@@ -11,8 +11,20 @@ import FirebaseFirestore
 final class StationViewModel: ObservableObject {
   
   // MARK: - Properties
+  
   @Published var stations: [Station] = []
+  @Published var sortType: StationSortType = .none
   private let stationService: StationService
+  
+  var sortedStations: [Station] {
+    switch sortType {
+    case .none: return stations
+    case .priceA95: return stations.sorted { $0.euroA95 < $1.euroA95 }
+    case .priceDP: return stations.sorted { $0.euroDP < $1.euroDP }
+    case .priceGas: return stations.sorted { $0.gas < $1.gas }
+    case .paymentMethods: return stations.sorted { $0.paymentMethods < $1.paymentMethods }
+    }
+  }
   
   // MARK: - Init
   
@@ -45,5 +57,15 @@ extension StationViewModel {
     let viewModel = StationViewModel()
     viewModel.stations = [MockData.station]
     return viewModel
+  }
+}
+
+extension StationViewModel {
+  enum StationSortType: String, CaseIterable {
+    case none = "None"
+    case priceA95 = "A95"
+    case priceDP = "DP"
+    case priceGas = "Gas"
+    case paymentMethods = "Payment Methods"
   }
 }
