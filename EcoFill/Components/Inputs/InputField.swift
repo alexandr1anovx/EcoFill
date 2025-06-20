e4b1b4b7f1561298a14e591501fb643474ef3b43
Alexander Andrianov <an4lex@gmail.com>
2025-06-20 19:12:19 +0300

    Improved the InputField struct.

--- EcoFill/Components/Inputs/InputField.swift
+++ EcoFill/Components/Inputs/InputField.swift
@@ -9,30 +9,68 @@ import SwiftUI
 
 struct InputField: View {
   
-  let inputType: InputContentType
+  private let inputType: InputContentType
+  private let validation: InputFieldValidation
   @Binding var inputData: String
+  @State private var isPasswordVisible = false
   
   init(
-    for inputType: InputContentType,
-    data: Binding<String>
+    _ inputType: InputContentType,
+    inputData: Binding<String>,
+    validation: InputFieldValidation = .none
   ) {
     self.inputType = inputType
-    self._inputData = data
+    self._inputData = inputData
+    self.validation = validation
   }
   
   var body: some View {
-    HStack(spacing: 15) {
+    HStack {
       Image(systemName: inputType.iconName)
         .frame(width: 18, height: 18)
         .foregroundStyle(.accent)
-        .opacity(0.8)
-      TextField(inputType.hint, text: $inputData)
-        .font(.subheadline)
+      Group {
+        if case .password = inputType, !isPasswordVisible {
+          SecureField(inputType.hint, text: $inputData)
+        } else {
+          TextField(inputType.hint, text: $inputData)
+        }
+      }
+      .textInputAutocapitalization(.never)
+      .autocorrectionDisabled(true)
+      .font(.subheadline)
+      .onChange(of: inputData) { _, newValue in
+        if inputType.shouldShowPasswordToggle {
+          if newValue.count > 20 {
+            inputData = String(newValue.prefix(20))
+          }
+        }
+      }
+      
+      if case .password = inputType {
+        Image(systemName: isPasswordVisible ? "eye" : "eye.slash")
+          .opacity(!inputData.isEmpty ? 1 : 0)
+          .disabled(inputData.isEmpty)
+          .foregroundStyle(.gray)
+          .onTapGesture {
+            isPasswordVisible.toggle()
+          }
+      }
+      
+      if case .passwordConfirmation(let matchingPassword) = validation {
+        Image(
+          systemName: inputData == matchingPassword && !inputData.isEmpty ? "checkmark.circle.fill" : "xmark.circle.fill"
+        )
+        .foregroundStyle(
+          inputData == matchingPassword && !inputData.isEmpty ? .green : .red
+        )
+        .opacity(!inputData.isEmpty ? 1 : 0)
+      }
     }
     .customInputFieldStyle()
   }
 }
 
 #Preview {
-  InputField(for: .password, data: .constant("Password"))
+  InputField(.password, inputData: .constant("Password"))
 }
