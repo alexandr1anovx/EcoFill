ea4a2c4a29856a5638c92669b876f7b37cbdc82d
Alexander Andrianov <an4lex@gmail.com>
2025-06-23 12:29:15 +0300

    Changed the behavior of the app launch when a login error occurs.

--- EcoFill/Features/Launch/LaunchScreen.swift
+++ EcoFill/Features/Launch/LaunchScreen.swift
@@ -3,53 +3,50 @@ import SwiftUI
 struct LaunchScreen: View {
   @State private var isShownLoadingAnimation = true
   @EnvironmentObject var authService: AuthenticationService
-  @EnvironmentObject var userService: UserService
   
   var body: some View {
     Group {
       switch authService.authState {
       case .signedIn(_):
         if isShownLoadingAnimation {
-          loadingBackground
+          loadingBackground()
         } else {
           TabBarView()
         }
       case .signedOut:
         LoginScreen()        
       case .loading:
-        loadingBackground
+        loadingBackground()
       case .error(let error):
-        Text("Loading Error: \(error.localizedDescription)")
-          .foregroundColor(.red)
+        loadingBackground(error: error)
       }
     }
     .onAppear {
       DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-        withAnimation(.spring) {
-          self.isShownLoadingAnimation = false
-        }
+        isShownLoadingAnimation = false
       }
     }
   }
   
   // MARK: - Subviews
   
-  private var loadingBackground: some View {
+  private func loadingBackground(error: Error? = nil) -> some View {
     ZStack {
       Color.appBackground.ignoresSafeArea()
       VStack {
-        Image(.logo)
         HStack {
           Text("Launching app...")
           ProgressView()
         }
+        if let error {
+          Text(error.localizedDescription)
+            .font(.footnote)
+            .foregroundStyle(.red)
+            .padding()
+          Text("Relaunch the app to try again.")
+            .font(.headline)
+        }
       }
     }
   }
-}
-
-#Preview {
-  LaunchScreen()
-    .environmentObject(AuthViewModel.previewMode)
-    .environmentObject(MapViewModel.previewMode)
 }
