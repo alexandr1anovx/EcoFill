24729a6f7a213a265b7a50f52f8d2c46b656d882
Alexander Andrianov <an4lex@gmail.com>
2025-06-26 22:26:12 +0300

    The launch screen was renamed to the RootView. Also changed its logic.

---
+++ EcoFill/Features/Root/RootView.swift
@@ -0,0 +1,57 @@
+//
+//  LaunchScreen.swift
+//  EcoFill
+//
+//  Created by Alexander Andrianov on 26.06.2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+  
+  @State private var isShownLoadingAnimation: Bool = true
+  @StateObject var sessionManager: SessionManager
+  let firebaseAuthService: AuthServiceProtocol
+  let firestoreUserService: UserServiceProtocol
+  
+  var body: some View {
+    Group {
+      switch sessionManager.sessionState {
+
+      case .loggedIn(_):
+        if isShownLoadingAnimation {
+          loadingBackgroundView()
+        } else {
+          TabBarView(
+            firebaseAuthService: firebaseAuthService,
+            firestoreUserService: firestoreUserService
+          )
+        }
+      
+      case .loggedOut:
+        LoginScreen(viewModel: LoginViewModel(authService: firebaseAuthService))
+      }
+    }
+    .animation(.easeInOut, value: sessionManager.sessionState)
+    .onAppear {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        isShownLoadingAnimation = false
+      }
+    }
+  }
+  
+  private func loadingBackgroundView() -> some View {
+    ZStack {
+      Color.appBackground.ignoresSafeArea()
+      VStack {
+        Image(.logo)
+          .resizable()
+          .frame(width: 120, height: 120)
+        HStack {
+          Text("Launching app...")
+          ProgressView()
+        }
+      }
+    }
+  }
+}
