cdbf0078df5e575c9a6cec04fa4d1ff512c8fd74
Alexander Andrianov <an4lex@gmail.com>
2025-06-27 20:03:48 +0300

    Minor improvements for the login view model.

--- EcoFill/Features/Authentication/ViewModels/LoginViewModel.swift
+++ EcoFill/Features/Authentication/ViewModels/LoginViewModel.swift
@@ -10,25 +10,19 @@ import Foundation
 @MainActor
 final class LoginViewModel: ObservableObject {
   
-  // MARK: - Public Properties
-  
   @Published var email: String = ""
   @Published var password: String = ""
   @Published var alertItem: AlertItem?
   @Published var isLoading: Bool = false
   
-  // MARK: - Private Properties
+  private let firebaseAuthService: AuthServiceProtocol
   
-  private let authService: AuthServiceProtocol
+  var isValidForm: Bool {
+    !email.isEmpty && !password.isEmpty
+  }
   
-  // MARK: - Computed Properties
-  
-  var isValidForm: Bool { !email.isEmpty && !password.isEmpty }
-  
-  // MARK: - Init
-  
-  init(authService: AuthServiceProtocol) {
-    self.authService = authService
+  init(firebaseAuthService: AuthServiceProtocol) {
+    self.firebaseAuthService = firebaseAuthService
   }
   
   // MARK: - Methods
@@ -36,7 +30,10 @@ final class LoginViewModel: ObservableObject {
   func signIn() async {
     isLoading = true
     do {
-      let _ = try await authService.signIn(email: email, password: password)
+      let _ = try await firebaseAuthService.signIn(
+        email: email,
+        password: password
+      )
     } catch {
       isLoading = false
       alertItem = AuthAlertContext.failedToLogin
